c3aa90c154c6a2b869ba6b85c92ef564c72e4a71
Maksim Pecherkin <maximal96@yandex.ru>
2023-02-12 14:01:49 +0300

    Lab 4 added

---
+++ ITMO.Cpp.Lab4.1/ITMO.Cpp.Lab4.1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <cmath>
+using namespace std;
+
+int Myroot(double, double, double, double&, double&);
+
+int Myroot(double a, double b, double c, double& x1, double& x2)
+{
+    double D = b * b - 4 * a * c;
+
+	if (D > 0) 
+	{
+		x1 = ((-b) + sqrt(D)) / (2 * a);
+		x2 = ((-b) - sqrt(D)) / (2 * a);
+		return 1;
+	}
+	else if (D == 0) 
+	{
+		x1 = (-b) / (2 * a);
+		x2 = x1;
+		return 0;
+	}
+	else if (D < 0)
+	{
+
+		return -1;
+	}
+}
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+	double a, b, c, x1 = 0, x2 = 0;
+
+	cout << "Введите коэфиценты квадратного уравнения: ";
+	cin >> a >> b >> c;
+
+	switch (Myroot(a, b, c, x1, x2))
+	{
+	case(1):
+		cout << "Уравнение имеет два корня: " << endl;
+		cout << "x1 = " << x1 << endl;
+		cout << "x2 = " << x2 << endl;
+		break;
+	case(0):
+		cout << "Уравнение имеет один корень: " << endl;
+		cout << "x = " << x1 << endl;
+		break;
+	case(-1):
+		cout << "Уравнение не имеет корней" << endl;
+		break;
+	}
+	
+}
---
+++ ITMO.Cpp.Lab4.2/ITMO.Cpp.Lab4.2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <cmath>
+using namespace std;
+
+bool Input(int& a, int& b)
+{
+    int x, y;
+
+    cout << "Введите 2 числа от 0 до 9: ";
+    cin >> x >> y;
+    if (((x < 0) || (x > 9)) || ((y < 0) || (y > 9)))
+    {
+        return false;
+    }
+
+    a = x;
+    b = y;
+
+    return true;
+}
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    int a, b;
+    if (Input(a, b) == false)
+    {
+        cerr << "error";
+        return 1;
+    }
+    int s = a + b;
+
+    return 0;
+}
